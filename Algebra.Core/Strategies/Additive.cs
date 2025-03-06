c8f611605da352f309f3df7fb762854a00ddc548
Pavel Oleinikov <node1212@gmail.com>
2025-03-06 19:17:25 +0200

    indentation

--- Algebra.Core/Strategies/Additive.cs
+++ Algebra.Core/Strategies/Additive.cs
@@ -2,17 +2,22 @@
 
 namespace Algebra.Core.Strategies
 {
-    public class AdditiveStrategy<TE> : IOperationStrategy<TE> where TE : IAdditionOperators<TE, TE, TE>
+    public class AdditiveStrategy<TE> : IOperationStrategy<TE>
+        where TE : IAdditionOperators<TE, TE, TE>
     {
         public TE Op(TE left, TE right) => left + right;
     }
 
-    public class AdditiveIdentityStrategy<TE> : AdditiveStrategy<TE>, IIdentityStrategy<TE> where TE : IAdditionOperators<TE, TE, TE>, IAdditiveIdentity<TE, TE>
+    public class AdditiveIdentityStrategy<TE> :
+        AdditiveStrategy<TE>, IIdentityStrategy<TE>
+        where TE : IAdditionOperators<TE, TE, TE>, IAdditiveIdentity<TE, TE>
     {
         public TE Identity => TE.AdditiveIdentity;
     }
 
-    public class AdditiveInverseStrategy<TE> : AdditiveIdentityStrategy<TE>, IInverseStrategy<TE> where TE : IAdditionOperators<TE, TE, TE>, IAdditiveIdentity<TE, TE>, IUnaryNegationOperators<TE, TE>
+    public class AdditiveInverseStrategy<TE> :
+        AdditiveIdentityStrategy<TE>, IInverseStrategy<TE>
+        where TE : IAdditionOperators<TE, TE, TE>, IAdditiveIdentity<TE, TE>, IUnaryNegationOperators<TE, TE>
     {
         public TE Inverse(TE element) => -element;
     }
--- Algebra.Core/Strategies/Multiplicative.cs
+++ Algebra.Core/Strategies/Multiplicative.cs
@@ -2,17 +2,22 @@
 
 namespace Algebra.Core.Strategies
 {
-    public class MultiplicativeStrategy<TE> : IOperationStrategy<TE> where TE : IMultiplyOperators<TE, TE, TE>
+    public class MultiplicativeStrategy<TE> : IOperationStrategy<TE>
+        where TE : IMultiplyOperators<TE, TE, TE>
     {
         public TE Op(TE left, TE right) => left * right;
     }
 
-    public class MultiplicativeIdentityStrategy<TE> : MultiplicativeStrategy<TE>, IIdentityStrategy<TE> where TE : IMultiplyOperators<TE, TE, TE>, IMultiplicativeIdentity<TE, TE>
+    public class MultiplicativeIdentityStrategy<TE> :
+        MultiplicativeStrategy<TE>, IIdentityStrategy<TE>
+        where TE : IMultiplyOperators<TE, TE, TE>, IMultiplicativeIdentity<TE, TE>
     {
         public TE Identity => TE.MultiplicativeIdentity;
     }
 
-    public class MultiplicativeInverseStrategy<TE> : MultiplicativeIdentityStrategy<TE>, IInverseStrategy<TE> where TE : IMultiplyOperators<TE, TE, TE>, IMultiplicativeIdentity<TE, TE>, IInversionOperator<TE, TE>
+    public class MultiplicativeInverseStrategy<TE> :
+        MultiplicativeIdentityStrategy<TE>, IInverseStrategy<TE>
+        where TE : IMultiplyOperators<TE, TE, TE>, IMultiplicativeIdentity<TE, TE>, IInversionOperator<TE, TE>
     {
         public TE Inverse(TE element) => ~element;
     }
